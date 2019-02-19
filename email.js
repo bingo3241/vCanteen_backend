78771efb97d4fec394551517ce36683a9935e6b1
Satsawat Suttawuttiwong <bingo3241@hotmail.com>
2019-02-19 19:04:10 +0700

    New Mailgun

--- email.js
+++ email.js
@@ -1,7 +1,7 @@
 module.exports = {
     mailto: function(password, email) {
-        var API_KEY = 'aeacd2e09129e2ffef44fb19cdd4da31-9ce9335e-8f70b4ae';
-        var DOMAIN = 'sandbox2d21c1642099472ebd59aa4c8e67858c.mailgun.org';
+        var API_KEY = process.env.MAILGUN_API_KEY;
+        var DOMAIN = process.env.MAILGUN_DOMAIN;
         var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
 
         const data = {
