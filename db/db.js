194da3c373183c3dede846e93bd18d57310c7912
Satsawat Suttawuttiwong <bingo3241@hotmail.com>
2019-03-16 18:20:07 +0700

    Implement Fixie Socks
    Add codes necessary for Fixie Socks

---
+++ db/db.js
@@ -0,0 +1,84 @@
+// var mysql = require('mysql2');
+
+// // //gcp db
+// var con = mysql.createConnection({
+//     host: "35.240.206.151",
+//     user: "root",
+//     password: "root",
+//     database: "test-db",
+// })
+
+// //local db
+// // var con = mysql.createConnection({
+// //     host: "localhost",
+// //     user: "root",
+// //     password: "root",
+// //     database: "testdb",
+// //     port: "8889"
+// // })
+
+// con.connect()
+
+// function query(sql, params = []) {
+//   return new Promise( (resolve, reject) => {
+//     con.query(sql, params, function(err, result, fields) {
+//         if (err) {
+//             reject(err)
+//         }
+//         resolve(result)
+//     })
+//   })
+// }
+
+
+//Fixie Socks (Only for HEROKU)
+'use strict';
+
+const SocksConnection = require('socksjs');
+const mysql = require('mysql2');
+const fixieUrl = process.env.FIXIE_SOCKS_HOST;
+const fixieValues = fixieUrl.split(new RegExp('[/(:\\/@)/]+'));
+
+const mysqlServer = {
+  host: '35.240.206.151'
+};
+
+const dbUser = 'root';
+const dbPassword = 'root';
+const db = 'test-db';
+
+const fixieConnection = new SocksConnection(mysqlServer, {
+  user: fixieValues[0],
+  pass: fixieValues[1],
+  host: fixieValues[2],
+  port: fixieValues[3],
+});
+
+const mysqlConnPool = mysql.createPool({
+  user: dbUser,
+  password: dbPassword,
+  database: db,
+  stream: fixieConnection
+});
+
+function query(sql, params = []) {
+  return new Promise( (resolve, reject) => {
+    mysqlConnPool.query(sql,params, function(err, rows, fields) {
+      // Connection is automatically released when query resolves
+      if(err) {
+        reject(err);
+      }
+      resolve(rows)
+   })
+  });
+}
+
+
+
+
+
+
+
+module.exports = {
+    query
+}
--- index.js
+++ index.js
@@ -1,25 +1,56 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var CryptoJS = require("crypto-js");
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+const customerModel = require('./models/customer');
+
+const passwordModule = require('./helpers/password');
+const emailModule = require('./helpers/email');
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/forget_password', function(req, res) {
-    const passwordModule = require('./random_password_generator');
-    const sjcl = require('./sjcl');
-    const emailModule = require('./email');
-    var newpassword = passwordModule.generate();
-    console.log('New password: '+newpassword);
-    var hash = sjcl.hash.sha256.hash(newpassword);
-    console.log('HASH :'+sjcl.codec.hex.fromBits(hash).toString().toUpperCase());
-    res.send('<h><b>This is /forget_password<b></h>')
-    // emailModule.mailto(newpassword, 'bingo6689@gmail.com');
-    
+// app.get('/forget_password', function(req, res) {
+//     // var email = req.body.email;
+//     var email = 'bingo6689@gmail.com';
+//     //GENERATE NEW PASSWORD
+//     var newpassword = passwordModule.generate();
+//     console.log('New password: '+newpassword);
+//     //HASHING NEW PASSWORD
+//     var hash = passwordModule.hash(newpassword);
+//     console.log('HASH :'+ passwordModule.toUpperCase(hash));
+//     res.send('<h><b>New password has been sent to your email<b></h>')
+//     //SENT PLAINTEXT NEW PASSWORD TO USER
+//     emailModule.mailto(newpassword, email);
+//     //TODO: INSERT HASHED NEW PASSWORD INTO DB
+// })
 
+app.put('/v1/user-authentication/customer/password/recover', (req,res) => {
+    var email = req.body.email;
+    if(customerModel.isInDatabase(email) == true) {
+        var newpassword = passwordModule.generate();
+        var hash = passwordModule.hash(newpassword);
+        emailModule.mailto(newpassword,email);
+        res.json(200, 'OK');
+    } else {
+        res.json(404, 'Email not found');
+    }
 })
+
+app.get('/customer', async (req,res) => {
+    res.json(await customerModel.getAll());
+})
+
+app.post('/sign_in', function(req, res) {
+    
+})
+
+
 
 io.on('connection', function(socket){
     console.log('a user connected :'+socket.id);
@@ -27,7 +58,7 @@ io.on('connection', function(socket){
   
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
 http.listen(port, () => {
     console.log(`Server listening on port ${port}`);
