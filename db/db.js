c023bf9a48ca49ac051ea8048c86237ccb4bda32
Unknown <supakit6948@gmail.com>
2019-03-28 19:00:22 +0700

    change to heroku

--- db/db.js
+++ db/db.js
@@ -76,3 +76,32 @@ function query(sql, params = []) {
 module.exports = {
     query
 }
+
+/*var mysql = require("mysql2");
+
+//local db
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "supakit6948",
+    password: "12345678",
+    database: "supakit6948",
+    port: 3306
+})
+
+con.connect()
+
+function query(sql, params = []) {
+    return new Promise( (resolve, reject) => {
+        con.query(sql, params, function(err, result, fields) {
+            if (err) {
+                reject(err)
+            }
+            resolve(result)
+        })
+    })
+    
+}
+
+module.exports = {
+    query
+}*/
