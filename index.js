8c4fe20490cc24c16ddce35fd80ce2e2e406a9dd
Unknown <supakit6948@gmail.com>
2019-03-28 19:02:38 +0700

    add function
    change pwd, view menu, view order

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 const customerModel = require('./models/customer');
-
+const vendorModel = require('./models/vendor');
 const passwordModule = require('./helpers/password');
 const emailModule = require('./helpers/email');
 
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html');
-});
+app.get('/', async (req, res) => {
+    //res.sendFile(__dirname + '/index.html');
+    res.json(await vendorModel.getAll());
+})
+
 
 // app.get('/forget_password', function(req, res) {
 //     // var email = req.body.email;
@@ -30,7 +32,7 @@ app.get('/', function(req, res){
 //     //TODO: INSERT HASHED NEW PASSWORD INTO DB
 // })
 
-app.put('/v1/user-authentication/customer/password/recover', (req,res) => {
+/*app.put('/v1/user-authentication/customer/password/recover', (req,res) => {
     var email = req.body.email;
     if(customerModel.isInDatabase(email) == true) {
         var newpassword = passwordModule.generate();
@@ -40,6 +42,56 @@ app.put('/v1/user-authentication/customer/password/recover', (req,res) => {
     } else {
         res.json(404, 'Email not found');
     }
+})*/
+
+app.put('/v1/user-authentication/customer/password/change' , async (req,res) => {
+    var customer_id = req.body.customerId;
+    console.log(customer_id);
+    var pwd = req.body.passwordNew;
+    console.log(pwd);
+    let [err, result] = await customerModel.changePassword(pwd,customer_id)
+    if (err) {
+        res.status(500).json(err)
+      } else if (result.affectedRows == 0){
+        res.status(404).send()
+      }else {
+        res.status(200).send()
+      }
+})
+
+app.put('/v1/vendor-main/:order_id/status', async (req,res) => {
+    let order_id = req.params.order_id;
+    console.log(order_id);
+    var order_status = req.body.order_status;
+    console.log(order_status);
+    let [err, result] = await vendorModel.updateOrderStatus(order_status,order_id)
+    if (err) {
+        res.status(500).json(err)
+      } else if (result.affectedRows == 0){
+        res.status(404).send()
+      }else {
+        res.status(200).send()
+      }
+})
+app.put('/v1/menu-management/vendorId/menu/' , async (req,res) => {
+    let vendor_id = req.body.vendor_id
+    let result = {combinationList: await vendorModel.getCombMenu(vendor_id), alacarteList: await vendorModel.getAlaMenu(vendor_id)}
+    if(result.combinationList == false || result.alacarteList == false) {
+      res.status(404).send()
+    } else {
+      res.json(result)
+    }
+     
+})
+
+app.post('/v1/vendor-main/vendorId/orders' , async(req,res) => {
+  let vendor_id = req.body.vendor_id
+  let result = {orderList : await vendorModel.getOrder(vendor_id)}
+  if(result.orderList == false){
+    res.status(404).send()
+  }else{
+    res.json(result)
+  }
 })
 
 app.get('/customer', async (req,res) => {
