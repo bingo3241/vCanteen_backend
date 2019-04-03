b7729e86e11ee3865794f1415b0457d1a9c2f012
New Akekamun <bingo3241@hotmail.com>
2019-04-03 16:13:51 +0700

    add firebase based on prai's branch

---
+++ app.js
@@ -0,0 +1,27 @@
+var config = {
+    apiKey: "AIzaSyD_ywwYWpPByXN6xXchGeFahXfIemmttQo",
+    authDomain: "vcanteen-d8ede.firebaseapp.com",
+    databaseURL: "https://vcanteen-d8ede.firebaseio.com",
+    projectId: "vcanteen-d8ede",
+    storageBucket: "vcanteen-d8ede.appspot.com",
+    messagingSenderId: "865311215719"
+  };
+
+  firebase.initializeApp(config);
+
+  firebase.auth.Auth.Persistence.LOCAL; 
+
+  $("#btn-login").click(function(){        
+    var email = $("#email").val();
+    var password = $("#password").val(); 
+
+    var result = firebase.auth().signInWithEmailAndPassword(email, password);
+    
+    result.catch(function(error){
+        var errorCode = error.code; 
+        var errorMessage = error.message; 
+
+        console.log(errorCode);
+        console.log(errorMessage);
+    });
+});
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
 
+const functions = require('firebase-functions');
+
+var {google} = require('googleapis');
+var MESSAGING_SCOPE = "https://www.googleapis.com/auth/firebase.messaging";
+var SCOPES = [MESSAGING_SCOPE];
+
+var router = express.Router(); 
+var request = require('request');
+
 const bodyParser = require('body-parser')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -382,6 +391,61 @@ app.get("/v1/orders/:cid/payment-method", async (req, res) => {
   let result = await ordersModel.getPaymentMethod(cid)
   res.json(result) 
 })
+
+router.post('/send', function(req, res){
+
+  getAccessToken().then(function(access_token){
+
+      var title = req.body.title; 
+      var body = req.body.body; 
+      var token = req.body.token; 
+
+      request.post({
+          headers:{
+              Authorization: 'Bearer '+access_token
+          }, 
+          url: "https://fcm.googleapis.com/v1/projects/vcanteen-d8ede/messages:send", 
+          body: JSON.stringify(
+              {
+                  "message":{
+                      "token" : token,
+                      "notification" : {
+                          "body" : body,
+                          "title" : title,
+                      }
+                  }
+              }
+          )
+      }, function(error, response, body){
+          res.end(body);
+          console.log(body);
+      });
+  });
+});
+
+app.use('/api', router);
+
+function getAccessToken(){
+  return new Promise(function(resolve, reject){
+      var key = require("./service-account.json");
+      var jwtClient = new google.auth.JWT(
+          key.client_email,
+          null,
+          key.private_key,
+          SCOPES,
+          null
+      );
+      jwtClient.authorize(function(err, tokens){
+          if(err){
+              reject(err);
+              return; 
+          }
+          resolve(tokens.access_token);
+      });
+  });
+}
+
+exports.api = functions.https.onRequest(app);
   
 let port = process.env.PORT;
 if (port == null || port == "") {
