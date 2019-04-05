f9ecb8831f1f2469361ffd42cf029010f856332b
Satsawat Suttawuttiwong <bingo3241@hotmail.com>
2019-04-05 15:19:54 +0700

    Re-add missing jwt

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ const db = require('./db/db')
 const passwordModule = require('./helpers/password');
 const emailModule = require('./helpers/email');
 
+const jwt = require('./library/jwt');
 
 const verifyJWT = (req, res, next) => {
   var token = req.body.token
